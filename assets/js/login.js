15816c57d18dd694a280b61e3d1f155cba2dc58d
Vitor Santos <pireshugo737@gmail.com>
2022-06-29 11:23:01 -0300

    'bug'

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,37 +15,27 @@ obj_allLogins [
     }
 ]
 */
-
-
-
-function loginChecker() {
-    let objPessoa = localStorage.getItem("pessoaLogada")
-    async function login() {
-        const email = document.getElementById("email");
-        const senha = document.getElementById("senha");
-
 async function login() {
     const email = document.getElementById("email");
     const senha = document.getElementById("senha");
 
-        const resposta = await fetch("https://codifica-demo-api.herokuapp.com/api/v1/users/login", {
-            method: "POST",
-            body: JSON.stringify({
-                email: email.value,
-                senha: senha.value
-            }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (resposta.ok) {
-            localStorage.setItem("loginAtual", email.value)
-            window.location.href = "telaProdutos.html"
-        } else {
-            alert("Email ou senha incorretos")
-            email.value = ""
-            senha.value = ""
+    const resposta = await fetch("https://codifica-demo-api.herokuapp.com/api/v1/users/login", {
+        method: "POST",
+        body: JSON.stringify({
+            email: email.value,
+            senha: senha.value
+        }),
+        headers: {
+            "Content-Type": "application/json"
         }
+    });
+
+    if (resposta.ok) {
+        localStorage.setItem("loginAtual", email.value)
+        window.location.href = "telaProdutos.html"
+    } else {
+        alert("Email ou senha incorretos")
+        email.value = ""
+        senha.value = ""
     }
 }
