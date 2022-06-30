3453b0582061ff2f668a6dcac83ed019d8c79952
Elias <pinheirinho2004@gmail.com>
2022-06-30 13:28:20 -0300

    mostrando todos os produtos e preços

--- assets/js/carrinho.js
+++ assets/js/carrinho.js
@@ -13,10 +13,28 @@ arrayCarrinho.forEach(element => {
     document.getElementsByClassName("all-produtos")[0].append(cloneAtual)
 });
 
+atualizarPrecos()
+
 
 
 function mudarQuantidade(element, valor) {
     if (parseInt(element.querySelector("p").innerHTML) + valor >= 1 && parseInt(element.querySelector("p").innerHTML) + valor <= 20) {
         element.querySelector("p").innerHTML = parseInt(element.querySelector("p").innerHTML) + valor
+        atualizarPrecos()
     }
+}
+
+function atualizarPrecos() {
+    const precos = document.getElementsByClassName("preco-unitario")
+    const quantidades = document.getElementsByClassName("quantidade-produto")
+    const total = document.getElementsByClassName("total-produto")
+    let totalCompra = document.getElementById("total-compra")
+
+    totalCompra.innerHTML = 0
+    for (let i = 0; i < total.length; i++) {
+        total[i].innerHTML = "R$ " + parseFloat(parseFloat(precos[i].innerHTML.slice(2).replace(",", ".")) * parseFloat(quantidades[i].innerHTML)).toFixed(2)
+
+        totalCompra.innerHTML = parseFloat(parseFloat(totalCompra.innerHTML) + parseFloat(total[i].innerHTML.slice(2).replace(",", "."))).toFixed(2)
+    }
+    totalCompra.innerHTML = "R$ " + totalCompra.innerHTML
 }
